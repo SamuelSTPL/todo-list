18c46ea095b44a0017309b4e7c2c8c735b9a5a70
SamuelSTPL <samuelstpierrelariviere@gmail.com>
2021-02-25 22:46:17 -0500

    Added vue-router + Axios and connected to json placeholder

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Header />
+    <router-view />
+  </div>
 </template>
 
 <script>
+import Header from "./components/layouts/Header";
 export default {
-  name: "App",
-  components: {}
+  name: "app",
+  components: {
+    Header
+  }
 };
 </script>
 
-<style></style>
+<style>
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+}
+.btn {
+  border: none;
+  background: #555;
+  color: #fff;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>About page</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
